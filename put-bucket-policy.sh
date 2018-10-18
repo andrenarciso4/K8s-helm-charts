7cc5ece6264c716e022e5444723e44d25cc9ee84
andrenarciso4 <andrenarciso4@gmail.com>
2018-10-18 10:13:39 +0000

    first commit

---
+++ put-bucket-policy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DEFAULT_REGION="eu-central-1"
+AWS_REGION="${AWS_REGION:-${DEFAULT_REGION}}"
+
+BUCKET="helm-20170102-8"
+NODE_ROLE_ARN="arn:aws:iam::211896161413:role/nodes.nosprojectk8s.cf"
+
+export AWS_REGION
+
+aws s3api put-bucket-policy --bucket ${BUCKET} --policy '{
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "AWS": "'${NODE_ROLE_ARN}'"
+          },
+         "Action": [
+          "s3:GetObject",
+          "s3:PutObject"
+          ],
+         "Resource": "arn:aws:s3:::'${BUCKET}'/*"
+      },
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "AWS": "'${NODE_ROLE_ARN}'"
+          },
+         "Action": "s3:ListBucket",
+         "Resource": "arn:aws:s3:::'${BUCKET}'"
+      }
+  ]
+}'
